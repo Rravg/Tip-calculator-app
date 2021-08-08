1c9136cea93f42e21dc95d582905324b7db99f0d
Rodrigo Villalobos <xravg.rodrigo@gmail.com>
2021-08-08 06:20:32 -0600

    add custom value option

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,6 +1,8 @@
 
 let total = document.getElementById('total');
-let tip_amount = document.getElementById('tip-amount');
+let tipAmount = document.getElementById('tip-amount');
+
+let customTip = document.getElementById('custom-tip');
 
 let tip5 = document.getElementById('5tip');
 let tip10 = document.getElementById('10tip');
@@ -14,11 +16,12 @@ let reset = document.getElementById('button');
 const request = () => {
     let bill = parseFloat(document.getElementById('bill').value);
     let people = parseInt(document.getElementById('people').value);
+    let custom =parseFloat(document.getElementById('custom-tip').value);
    
-    return [bill, people];
+    return [bill, people, custom];
 }
 
-const requestActive = (reset) => {
+const requestActive = () => {
     reset.disabled = false;
     reset.style.color = 'hsl(183, 100%, 15%)';
     reset.style.backgroundColor = 'hsl(172, 67%, 45%)';
@@ -27,7 +30,6 @@ const requestActive = (reset) => {
 
 const resetButton = function () {
 
-    
     tip5.style.backgroundColor = 'hsl(183, 100%, 15%)';
     tip5.style.color = 'white';
     tip10.style.backgroundColor = 'hsl(183, 100%, 15%)';
@@ -41,9 +43,10 @@ const resetButton = function () {
 
     document.getElementById('bill').value = NaN;
     document.getElementById('people').value = NaN;
+    document.getElementById('custom-tip').value = NaN;
 
     total.innerHTML = '$0.00';
-    tip_amount.innerHTML = '$0.00';
+    tipAmount.innerHTML = '$0.00';
     reset.style.color = 'default'
     reset.style.backgroundColor = 'default';
     reset.disabled = true;
@@ -52,7 +55,7 @@ const resetButton = function () {
 }
 
 let clickedButton = function(event) {
-    let [bill, people] = request();
+    let [bill, people, custom] = request();
     
 
     if (people === 0) {
@@ -62,10 +65,10 @@ let clickedButton = function(event) {
     else if (!Number.isNaN(people) && !Number.isNaN(bill)) {
         event.target.style.color = 'hsl(186, 14%, 43%)';
         event.target.style.backgroundColor = 'hsl(185, 41%, 84%)';
-        requestActive(reset);
+        requestActive();
         alert.style.color = 'transparent';
 
-        if (event.target === tip5) {
+        if (event.target === tip5 && Number.isNaN(custom)) {
 
             tip10.style.backgroundColor = 'hsl(183, 100%, 15%)';
             tip10.style.color = 'white';
@@ -78,13 +81,13 @@ let clickedButton = function(event) {
 
             let tip = (bill * 0.05) / people;
             tip = tip.toFixed(2);
-            tip_amount.innerHTML = `$${tip}`;
+            tipAmount.innerHTML = `$${tip}`;
         
             bill = (bill * 1.05 ) / people;
             bill = bill.toFixed(2);
             total.innerHTML = `$${bill}`;  
 
-        } else if (event.target === tip10) {
+        } else if (event.target === tip10 && Number.isNaN(custom)) {
 
             tip5.style.backgroundColor = 'hsl(183, 100%, 15%)';
             tip5.style.color = 'white';
@@ -97,13 +100,13 @@ let clickedButton = function(event) {
 
             let tip = (bill * 0.10) / people;
             tip = tip.toFixed(2);
-            tip_amount.innerHTML = `$${tip}`;
+            tipAmount.innerHTML = `$${tip}`;
         
             bill = (bill * 1.10 ) / people;
             bill = bill.toFixed(2);
             total.innerHTML = `$${bill}`; 
 
-        } else if (event.target === tip15) {
+        } else if (event.target === tip15 && Number.isNaN(custom)) {
 
             tip10.style.backgroundColor = 'hsl(183, 100%, 15%)';
             tip10.style.color = 'white';
@@ -116,13 +119,13 @@ let clickedButton = function(event) {
 
             let tip = (bill * 0.15) / people;
             tip = tip.toFixed(2);
-            tip_amount.innerHTML = `$${tip}`;
+            tipAmount.innerHTML = `$${tip}`;
         
             bill = (bill * 1.15 ) / people;
             bill = bill.toFixed(2);
             total.innerHTML = `$${bill}`; 
 
-        } else if (event.target === tip25) {
+        } else if (event.target === tip25 && Number.isNaN(custom)) {
 
             tip10.style.backgroundColor = 'hsl(183, 100%, 15%)';
             tip10.style.color = 'white';
@@ -135,13 +138,13 @@ let clickedButton = function(event) {
 
             let tip = (bill * 0.25) / people;
             tip = tip.toFixed(2);
-            tip_amount.innerHTML = `$${tip}`;
+            tipAmount.innerHTML = `$${tip}`;
         
             bill = (bill * 1.25 ) / people;
             bill = bill.toFixed(2);
             total.innerHTML = `$${bill}`; 
 
-        } else if (event.target === tip50) {
+        } else if (event.target === tip50 && Number.isNaN(custom)) {
 
             tip10.style.backgroundColor = 'hsl(183, 100%, 15%)';
             tip10.style.color = 'white';
@@ -154,12 +157,33 @@ let clickedButton = function(event) {
 
             let tip = (bill * 0.50) / people;
             tip = tip.toFixed(2);
-            tip_amount.innerHTML = `$${tip}`;
+            tipAmount.innerHTML = `$${tip}`;
         
             bill = (bill * 1.50 ) / people;
             bill = bill.toFixed(2);
             total.innerHTML = `$${bill}`; 
-        }
+        } else if (!Number.isNaN(custom)) {
+            let customValue = custom;
+
+            tip5.style.backgroundColor = 'hsl(183, 100%, 15%)';
+            tip5.style.color = 'white';
+            tip10.style.backgroundColor = 'hsl(183, 100%, 15%)';
+            tip10.style.color = 'white';
+            tip15.style.backgroundColor = 'hsl(183, 100%, 15%)';
+            tip15.style.color = 'white';
+            tip25.style.backgroundColor = 'hsl(183, 100%, 15%)';
+            tip25.style.color = 'white';
+            tip50.style.backgroundColor = 'hsl(183, 100%, 15%)';
+            tip50.style.color = 'white';
+
+            let tip = (bill * (customValue/100)) / people;
+            tip = tip.toFixed(2);
+            tipAmount.innerHTML = `$${tip}`;
+        
+            bill = (bill * ((customValue/100) + 1)) / people;
+            bill = bill.toFixed(2);
+            total.innerHTML = `$${bill}`; 
+        }  
  
     }  
 }  
@@ -169,5 +193,7 @@ tip10.addEventListener('click',clickedButton);
 tip15.addEventListener('click', clickedButton);
 tip25.addEventListener('click', clickedButton);
 tip50.addEventListener('click', clickedButton);
+
+customTip.addEventListener('click', clickedButton);
 
 reset.addEventListener('click', resetButton);
